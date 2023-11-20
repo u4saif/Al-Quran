4e1216705df7604adac3a091762a78c0c81cb185
eysaif <u4saif@gmail.com>
2023-11-20 22:12:14 +0530

    top and side nav created with css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
