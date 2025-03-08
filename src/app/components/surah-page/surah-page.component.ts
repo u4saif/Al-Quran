163431daf83041b1f4f9f5d86639dad90cf8106e
Saifuddin <shaikh.saifuddin@internationalsos.com>
2025-03-08 15:23:15 +0530

    feature auto scroll added

--- src/app/components/surah-page/surah-page.component.ts
+++ src/app/components/surah-page/surah-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-surah-page',
@@ -9,6 +9,7 @@ export class SurahPageComponent implements OnInit, OnChanges {
   bismillah: string =
     '\ufeff\u0628\u0650\u0633\u0652\u0645\u0650 \u0627\u0644\u0644\u0651\u064e\u0647\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650';
   @Input() surah: any;
+  @ViewChildren('listItem') listItems!: QueryList<ElementRef>;
   audioURL: string = '';
   showAudioPlayer: boolean = false;
   currentAyatPlaying: any = {
@@ -36,6 +37,13 @@ export class SurahPageComponent implements OnInit, OnChanges {
     };
     this.showAudioPlayer = true;
     this.audioURL = item.audio;
+    const ayatPlaying = this.listItems.toArray()[ayatNumber-1];
+    if (ayatPlaying) {
+      ayatPlaying.nativeElement.style.backgroundColor = '#fff00054';
+      ayatPlaying.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    } else {
+      console.log(`The ${ayatNumber}th <li> does not exist.`);
+    }
   }
 
   nextAyatPlayed(event:any){ 
